ecaa2994eed26a44d723594cb530e2a067f190ad
hero534 <goldsmhr@mail.uc.edu>
2025-03-01 19:30:35 -0500

    Assignment Completed

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,42 @@ public class Main {
         int die1 = 0;
         int die2 = 0;
         int cRoll = 0;
-        String Play = "";
-        boolean done = false;
-            while (!done) {
-                die1 = rnd.nextInt(6) + 1;
-                die2 = rnd.nextInt(6) + 1;
-                cRoll = die1 + die2;
-                System.out.println("Roll the dice: ");
-                    if (cRoll == 2 || cRoll == 3 || cRoll == 12) {
-                        System.out.println("With the first dice being " + die1 + " and the second die being " + die2 + " the sum will be " + cRoll + " which means you are crapping out.");
-                    } else if (cRoll == 7 || cRoll == 11) {
-                        System.out.println("With the first dice being " + die1 + " and the second die being " + die2 + " the sum will be " + cRoll + " which means you are a natural!");
-                    } else {
-                        System.out.println("With the first dice being " + die1 + " and the second die being " + die2 + " the sum will be " + cRoll + " which means you need to roll again!");
-                    }
-                System.out.println("Do you want to play again? If Yes then type Y, if No then type N: ");
-                    Play = in.nextLine();
-                    if (Play.equals ("Y")) {
-                        done = false;
-                    }else if (Play.equals ("N")) {
-                        done = true;
-                    }
+        int point = 0;
+        String Play = "Y";
+            while (Play.equals ("Y")) {
+                System.out.println("Press enter to roll the dice...");
+                in.nextLine();
+                    die1 = rnd.nextInt(6) + 1;
+                    die2 = rnd.nextInt(6) + 1;
+                    cRoll = die1 + die2;
+                    System.out.println("You rolled " + die1 + " + " + die2 + " to equal " + cRoll);
+                        if (cRoll == 2 || cRoll == 3 || cRoll == 12) {
+                            System.out.println("Craps, You lose.");
+                        } else if (cRoll == 7 || cRoll == 11) {
+                            System.out.println("You're a natural, You win!");
+                        } else {
+                            point = cRoll;
+                            System.out.println("Point is set to " + point);
+                                while (true) {
+                                    die1 = rnd.nextInt(6) + 1;
+                                    die2 = rnd.nextInt(6) + 1;
+                                    cRoll = die1 + die2;
+                                    System.out.println("Press enter to roll again...");
+                                    in.nextLine();
+                                        if (cRoll == point) {
+                                            System.out.println("You win!");
+                                            point = 0;
+                                            break;
+                                        }else if (cRoll == 7) {
+                                            System.out.println("You rolled a 7! You lose.");
+                                            point = 0;
+                                            break;
+                                        }
+                                }
+                        }
+                    System.out.println("Do you want to play again? If Yes then type Y, if No then type N: ");
+                        Play = in.nextLine();
             }
+            in.close();
     }
 }
